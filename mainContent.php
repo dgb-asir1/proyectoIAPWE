578ef2c5d1089106b5ae19ed8d8033e341eccdac
dgb-asir1 <damasogb58@educastur.es>
2025-11-09 22:23:04 +0100

    cambios estéticos

--- mainContent.php
+++ mainContent.php
@@ -15,45 +15,45 @@ $spanishTexts = [
 <html>
 
 <head>
-    <title>LABUBAI</title>
+    <title>SIDRACOLA</title>
     <link rel="stylesheet" href="css/allStyles.css">
 </head>
 
 <body>
     <nav id="nav1">
         <div id="languageBtns">
-            <button id="langBtnEs"></button>
-            <button id="langBtnEng"></button>
-            <button id="langBtnFr"></button>
+            <button id="langBtnEs"><img src="./img/flag_Spain.png"></img></button>
+            <button id="langBtnEng"><img src="./img/flag_USA.png"></img></button>
+            <button id="langBtnFr"><img src="./img/flag_France.png"></img></button>
         </div>
-        <div id="logoutBtnDiv">
-            <button id="">LOGOUT</button>
+        <div id="logoutBtn">
+            <button id="">SALIR</button>
         </div>
     </nav>
     <nav>
-        <div id="logo"><img src="./img/logo.png"></div>
+        <div id="logo"><button><img src="./img/logo.png"></img></button></div>
         <div id="mainBtns">
             <button>INICIO</button>
             <button>CONTACTO</button>
         </div>
-        <div id="themeBtns">
-            <button><img src="./img/themeSwitch.png"></button>
+        <div id="themeBtn">
+            <button><img src="./img/themeSwitch.png"></img></button>
         </div>
     </nav>
     <section id="mainImg">
-        <img></img>
+        <img src="img/sidracola.jpg"></img>
     </section>
     <section id="mainText">
         <details>
-            <summary><h2>NUESTRA EMPRESA</h2></summary>
+            <summary><h2>Nosotros</h2></summary>
             <p>Here is the content!</p>
         </details>
         <details>
-            <summary><h2>NUESTRO PRODUCTO</h2></summary>
+            <summary><h2>Nuestro producto</h2></summary>
             <p>Here is the content!</p>
         </details>
         <details>
-            <summary><h2>NUESTRO PRODUCTO</h2></summary>
+            <summary><h2>Ven a vernos</h2></summary>
             <p>Here is the content!</p>
         </details>
     </section>
