2b1925f2176174234794d72499983cf439516736
dgb-asir1 <damasogb58@educastur.es>
2025-11-13 21:02:48 +0100

    traducciones

--- mainContent.php
+++ mainContent.php
@@ -1,13 +1,48 @@
 <?php
 
-#$userLogged;
-
-$language = "ESP";
-
-$spanishTexts = [
+$textSpanish = [
     "inicio" => "Inicio",
-    "info" => "Informaci..."
+    "info" => "Información",
+    "nosotros" => "Nosotros",
+    "nuestro_producto" => "Nuestro producto",
+    "ven_a_vernos" => "Ven a vernos",
+    "salir" => "Salir",
+    "cuestionario" => "Cuestionario"
 ];
+
+$textEnglish = [
+    "inicio" => "Home",
+    "info" => "Information",
+    "nosotros" => "About Us",
+    "nuestro_producto" => "Our Product",
+    "ven_a_vernos" => "Come Visit Us",
+    "salir" => "Logout",
+    "cuestionario" => "Quiz"
+];
+
+$textFrench = [
+    "inicio" => "Accueil",
+    "info" => "Information",
+    "nosotros" => "À propos de nous",
+    "nuestro_producto" => "Notre produit",
+    "ven_a_vernos" => "Venez nous voir",
+    "salir" => "Déconnexion",
+    "cuestionario" => "Questionnaire"
+];
+
+if (isset($_GET['lang'])) {
+    $language = $_GET['lang'];
+} else {
+    $language = 'ESP';
+}
+
+if ($language == "ESP") {
+    $texts = $textSpanish;
+} elseif ($language == "ENG") {
+    $texts = $textEnglish;
+} elseif ($language == "FRA") {
+    $texts = $textFrench;
+} 
 
 
 ?>
@@ -22,12 +57,22 @@ $spanishTexts = [
 <body>
     <nav id="nav1">
         <div id="languageBtns">
-            <button id="langBtnEs"><img src="./img/flag_Spain.png"></img></button>
-            <button id="langBtnEng"><img src="./img/flag_USA.png"></img></button>
-            <button id="langBtnFr"><img src="./img/flag_France.png"></img></button>
+            <form action="" method="get">
+                <input type="hidden" name="lang" value="ESP">
+                <button id="langBtnEs" type="submit"><img src="./img/flag_Spain.png"></button>
+            </form>
+
+            <form action="" method="get">
+                <input type="hidden" name="lang" value="ENG">
+                <button id="langBtnEng" type="submit"><img src="./img/flag_USA.png"></button>
+            </form>
+
+            <form action="" method="get">
+                <input type="hidden" name="lang" value="FRA">
+                <button id="langBtnFr" type="submit"><img src="./img/flag_France.png"></button>
+            </form>
         </div>
-        <div id="logoutBtn">
-            <button id=""><a href="index.php">SALIR</a></button>
+
         </div>
     </nav>
     <nav>
@@ -45,15 +90,21 @@ $spanishTexts = [
     </section>
     <section id="mainText">
         <details>
-            <summary><h2>Nosotros</h2></summary>
+            <summary>
+                <h2><?php echo $texts['nosotros'] ?></h2>
+            </summary>
             <p>Here is the content!</p>
         </details>
         <details>
-            <summary><h2>Nuestro producto</h2></summary>
+            <summary>
+                <h2>Nuestro producto</h2>
+            </summary>
             <p>Here is the content!</p>
         </details>
         <details>
-            <summary><h2>Ven a vernos</h2></summary>
+            <summary>
+                <h2>Ven a vernos</h2>
+            </summary>
             <p>Here is the content!</p>
         </details>
     </section>
