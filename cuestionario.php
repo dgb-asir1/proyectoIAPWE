cb1e4ebcb715eef30bf970e0f7a4514db24bff3f
dgb-asir1 <damasogb58@educastur.es>
2025-11-15 17:22:13 +0100

    traducido el cuestionario

--- cuestionario.php
+++ cuestionario.php
@@ -10,70 +10,70 @@ $nota = 0;
 
 if (!empty($_POST['pregunta1'])) {
     $respuesta1 = $_POST['pregunta1'];
-    if ($respuesta1 == "GET") {
+    if ($respuesta1 == "resp_b") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta2'])) {
     $respuesta2 = $_POST['pregunta2'];
-    if ($respuesta2 == "") {
+    if ($respuesta2 == "resp_a") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta3'])) {
     $respuesta3 = $_POST['pregunta3'];
-    if ($respuesta3 == "") {
+    if ($respuesta3 == "resp_b") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta4'])) {
     $respuesta4 = $_POST['pregunta4'];
-    if ($respuesta4 == "") {
+    if ($respuesta4 == "resp_c") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta5'])) {
     $respuesta5 = $_POST['pregunta5'];
-    if ($respuesta5 == "") {
+    if ($respuesta5 == "resp_a") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta6'])) {
     $respuesta6 = $_POST['pregunta6'];
-    if ($respuesta6 == "") {
+    if ($respuesta6 == "resp_c") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta7'])) {
     $respuesta7 = $_POST['pregunta7'];
-    if ($respuesta7 == "") {
+    if ($respuesta7 == "resp_b") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta8'])) {
     $respuesta8 = $_POST['pregunta8'];
-    if ($respuesta8 == "") {
+    if ($respuesta8 == "resp_b") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta9'])) {
     $respuesta9 = $_POST['pregunta9'];
-    if ($respuesta9 == "") {
+    if ($respuesta9 == "resp_a") {
         $nota++;
     }
 }
 
 if (!empty($_POST['pregunta10'])) {
     $respuesta10 = $_POST['pregunta10'];
-    if ($respuesta10 == "") {
+    if ($respuesta10 == "resp_c") {
         $nota++;
     }
 }
@@ -122,111 +122,129 @@ if (!empty($_POST['pregunta10'])) {
         </div>
         <div id="themeBtn">
             <form action="" method="get">
-                <input type="hidden" name="theme" value=
-                    <?php 
-                    if(isset($_GET['theme'])){
-                        $_GET['theme'] == 'LIGHT' ? $themeBtnValue = "DARK" : $themeBtnValue = "LIGHT";   
-                    }
-                    else {
-                        $themeBtnValue = "DARK";
-                    }
-                    echo $themeBtnValue;
-                    ?>>
+                <input type="hidden" name="theme" value=<?php
+                                                        if (isset($_GET['theme'])) {
+                                                            $_GET['theme'] == 'LIGHT' ? $themeBtnValue = "DARK" : $themeBtnValue = "LIGHT";
+                                                        } else {
+                                                            $themeBtnValue = "DARK";
+                                                        }
+                                                        echo $themeBtnValue;
+                                                        ?>>
                 <button type="submit"><img src="./img/themeSwitch.png"></img></button>
             </form>
         </div>
-    </nav>    
+    </nav>
+
     <section id="formContainer">
-        <h2>Cuestionario</h2>
+        <h2><?php echo $text_quiz['títuloCuestionario'] ?></h2>
         <form action="cuestionario.php" method="POST">
             <fieldset>
-                <legend>Pregunta 1</legend>
-                <p>¿Cuál de los dos protocolos envía en texto plano?</p>
-                <input type="radio" id="pregunta1_a" name="pregunta1" value="GET">
-                <label for="pregunta1_a">GET</label>
-                <input type="radio" id="pregunta1_b" name="pregunta1" value="POST">
-                <label for="pregunta1_b">POST</label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 1</legend>
+                <p><?php echo $text_quiz['pregunta1'] ?></p>
+                <input type="radio" id="pregunta1_resp_a" name="pregunta1" value="resp_a">
+                <label for="pregunta1_resp_a"><?php echo $text_quiz['pregunta1_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta1_resp_b" name="pregunta1" value="resp_b">
+                <label for="pregunta1_resp_b"><?php echo $text_quiz['pregunta1_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta1_resp_c" name="pregunta1" value="resp_c">
+                <label for="pregunta1_resp_c"><?php echo $text_quiz['pregunta1_resp_c'] ?></label><br>
+            </fieldset>
+            <fieldset>
+                <legend><?php echo $text_quiz['pregunta'] ?> 2</legend>
+                <p><?php echo $text_quiz['pregunta2'] ?></p>
+                <input type="radio" id="pregunta2_resp_a" name="pregunta2" value="resp_a">
+                <label for="pregunta2_resp_a"><?php echo $text_quiz['pregunta2_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta2_resp_b" name="pregunta2" value="resp_b">
+                <label for="pregunta2_resp_b"><?php echo $text_quiz['pregunta2_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta2_resp_c" name="pregunta2" value="resp_c">
+                <label for="pregunta2_resp_c"><?php echo $text_quiz['pregunta2_resp_c'] ?></label><br>
             </fieldset>
 
             <fieldset>
-                <legend>Pregunta 2</legend>
-                <input type="radio" id="pregunta2_a" name="pregunta2" value="opcion_a">
-                <label for="pregunta2_a"></label>
-
-                <input type="radio" id="pregunta2_b" name="pregunta2" value="opcion_b">
-                <label for="pregunta2_b"></label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 3</legend>
+                <p><?php echo $text_quiz['pregunta3'] ?></p>
+                <input type="radio" id="pregunta3_resp_a" name="pregunta3" value="resp_a">
+                <label for="pregunta3_resp_a"><?php echo $text_quiz['pregunta3_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta3_resp_b" name="pregunta3" value="resp_b">
+                <label for="pregunta3_resp_b"><?php echo $text_quiz['pregunta3_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta3_resp_c" name="pregunta3" value="resp_c">
+                <label for="pregunta3_resp_c"><?php echo $text_quiz['pregunta3_resp_c'] ?></label><br>
             </fieldset>
 
             <fieldset>
-                <legend>Pregunta 3</legend>
-                <input type="radio" id="pregunta3_a" name="pregunta3" value="opcion_a">
-                <label for="pregunta3_a"></label>
-
-                <input type="radio" id="pregunta3_b" name="pregunta3" value="opcion_b">
-                <label for="pregunta3_b"></label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 4</legend>
+                <p><?php echo $text_quiz['pregunta4'] ?></p>
+                <input type="radio" id="pregunta4_resp_a" name="pregunta4" value="resp_a">
+                <label for="pregunta4_resp_a"><?php echo $text_quiz['pregunta4_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta4_resp_b" name="pregunta4" value="resp_b">
+                <label for="pregunta4_resp_b"><?php echo $text_quiz['pregunta4_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta4_resp_c" name="pregunta4" value="resp_c">
+                <label for="pregunta4_resp_c"><?php echo $text_quiz['pregunta4_resp_c'] ?></label><br>
             </fieldset>
 
             <fieldset>
-                <legend>Pregunta 4</legend>
-                <input type="radio" id="pregunta4_a" name="pregunta4" value="opcion_a">
-                <label for="pregunta4_a"></label>
-
-                <input type="radio" id="pregunta4_b" name="pregunta4" value="opcion_b">
-                <label for="pregunta4_b"></label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 5</legend>
+                <p><?php echo $text_quiz['pregunta5'] ?></p>
+                <input type="radio" id="pregunta5_resp_a" name="pregunta5" value="resp_a">
+                <label for="pregunta5_resp_a"><?php echo $text_quiz['pregunta5_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta5_resp_b" name="pregunta5" value="resp_b">
+                <label for="pregunta5_resp_b"><?php echo $text_quiz['pregunta5_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta5_resp_c" name="pregunta5" value="resp_c">
+                <label for="pregunta5_resp_c"><?php echo $text_quiz['pregunta5_resp_c'] ?></label><br>
             </fieldset>
 
             <fieldset>
-                <legend>Pregunta 5</legend>
-                <input type="radio" id="pregunta5_a" name="pregunta5" value="opcion_a">
-                <label for="pregunta5_a"></label>
-
-                <input type="radio" id="pregunta5_b" name="pregunta5" value="opcion_b">
-                <label for="pregunta5_b"></label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 6</legend>
+                <p><?php echo $text_quiz['pregunta6'] ?></p>
+                <input type="radio" id="pregunta6_resp_a" name="pregunta6" value="resp_a">
+                <label for="pregunta6_resp_a"><?php echo $text_quiz['pregunta6_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta6_resp_b" name="pregunta6" value="resp_b">
+                <label for="pregunta6_resp_b"><?php echo $text_quiz['pregunta6_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta6_resp_c" name="pregunta6" value="resp_c">
+                <label for="pregunta6_resp_c"><?php echo $text_quiz['pregunta6_resp_c'] ?></label><br>
             </fieldset>
 
             <fieldset>
-                <legend>Pregunta 6</legend>
-                <input type="radio" id="pregunta6_a" name="pregunta6" value="opcion_a">
-                <label for="pregunta6_a"></label>
-
-                <input type="radio" id="pregunta6_b" name="pregunta6" value="opcion_b">
-                <label for="pregunta6_b"></label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 7</legend>
+                <p><?php echo $text_quiz['pregunta7'] ?></p>
+                <input type="radio" id="pregunta7_resp_a" name="pregunta7" value="resp_a">
+                <label for="pregunta7_resp_a"><?php echo $text_quiz['pregunta7_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta7_resp_b" name="pregunta7" value="resp_b">
+                <label for="pregunta7_resp_b"><?php echo $text_quiz['pregunta7_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta7_resp_c" name="pregunta7" value="resp_c">
+                <label for="pregunta7_resp_c"><?php echo $text_quiz['pregunta7_resp_c'] ?></label><br>
             </fieldset>
 
             <fieldset>
-                <legend>Pregunta 7</legend>
-                <input type="radio" id="pregunta7_a" name="pregunta7" value="opcion_a">
-                <label for="pregunta7_a"></label>
-
-                <input type="radio" id="pregunta7_b" name="pregunta7" value="opcion_b">
-                <label for="pregunta7_b"></label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 8</legend>
+                <p><?php echo $text_quiz['pregunta8'] ?></p>
+                <input type="radio" id="pregunta8_resp_a" name="pregunta8" value="resp_a">
+                <label for="pregunta8_resp_a"><?php echo $text_quiz['pregunta8_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta8_resp_b" name="pregunta8" value="resp_b">
+                <label for="pregunta8_resp_b"><?php echo $text_quiz['pregunta8_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta8_resp_c" name="pregunta8" value="resp_c">
+                <label for="pregunta8_resp_c"><?php echo $text_quiz['pregunta8_resp_c'] ?></label><br>
             </fieldset>
 
             <fieldset>
-                <legend>Pregunta 8</legend>
-                <input type="radio" id="pregunta8_a" name="pregunta8" value="opcion_a">
-                <label for="pregunta8_a"></label>
-
-                <input type="radio" id="pregunta8_b" name="pregunta8" value="opcion_b">
-                <label for="pregunta8_b"></label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 9</legend>
+                <p><?php echo $text_quiz['pregunta9'] ?></p>
+                <input type="radio" id="pregunta9_resp_a" name="pregunta9" value="resp_a">
+                <label for="pregunta9_resp_a"><?php echo $text_quiz['pregunta9_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta9_resp_b" name="pregunta9" value="resp_b">
+                <label for="pregunta9_resp_b"><?php echo $text_quiz['pregunta9_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta9_resp_c" name="pregunta9" value="resp_c">
+                <label for="pregunta9_resp_c"><?php echo $text_quiz['pregunta9_resp_c'] ?></label><br>
             </fieldset>
 
             <fieldset>
-                <legend>Pregunta 9</legend>
-                <input type="radio" id="pregunta9_a" name="pregunta9" value="opcion_a">
-                <label for="pregunta9_a"></label>
-
-                <input type="radio" id="pregunta9_b" name="pregunta9" value="opcion_b">
-                <label for="pregunta9_b"></label>
-            </fieldset>
-
-            <fieldset>
-                <legend>Pregunta 10</legend>
-                <input type="radio" id="pregunta10_a" name="pregunta10" value="opcion_a">
-                <label for="pregunta10_a"></label>
-
-                <input type="radio" id="pregunta10_b" name="pregunta10" value="opcion_b">
-                <label for="pregunta10_b"></label>
+                <legend><?php echo $text_quiz['pregunta'] ?> 10</legend>
+                <p><?php echo $text_quiz['pregunta10'] ?></p>
+                <input type="radio" id="pregunta10_resp_a" name="pregunta10" value="resp_a">
+                <label for="pregunta10_resp_a"><?php echo $text_quiz['pregunta10_resp_a'] ?></label><br>
+                <input type="radio" id="pregunta10_resp_b" name="pregunta10" value="resp_b">
+                <label for="pregunta10_resp_b"><?php echo $text_quiz['pregunta10_resp_b'] ?></label><br>
+                <input type="radio" id="pregunta10_resp_c" name="pregunta10" value="resp_c">
+                <label for="pregunta10_resp_c"><?php echo $text_quiz['pregunta10_resp_c'] ?></label><br>
             </fieldset>
 
             <button type="submit">Corregir</button>
@@ -236,9 +254,9 @@ if (!empty($_POST['pregunta10'])) {
     <section>
         <p>
             <?php
-                if($nota!==0){
-                    echo "Has sacado una nota de $nota";
-                }
+            if ($nota !== 0) {
+                echo "Has sacado una nota de $nota";
+            }
             ?>
         </p>
     </section>
--- translations/translations.php
+++ translations/translations.php
@@ -10,34 +10,89 @@ $text_index_spanish = [
 ];
 
 $text_main_spanish = [
-    "salir" => "Salir",    
+    "salir" => "Salir",
     "inicio" => "Inicio",
-    "cuestionario" => "Cuestionario",    
+    "cuestionario" => "Cuestionario",
     "info" => "Información",
     "nosotros" => "Nosotros",
     "nosotros_contenido" => "Javi y Marta son dos jóvenes emprendedores con formación en Química y Empresariales. Su pasión por la innovación y la sostenibilidad los llevó a crear Sidracola, una bebida natural y única.",
     "nuestro_producto" => "Nuestro producto",
-    "nuestro_producto_contenido" => "Sidracola fusiona el refrescante sabor de la cola con la suavidad de la sidra, creando una experiencia única y 100% natural. Es el resultado de años de investigación y un proceso de fabricación comprometido con la calidad.",    
+    "nuestro_producto_contenido" => "Sidracola fusiona el refrescante sabor de la cola con la suavidad de la sidra, creando una experiencia única y 100% natural. Es el resultado de años de investigación y un proceso de fabricación comprometido con la calidad.",
     "ven_a_vernos" => "Ven a vernos",
     "ven_a_vernos_contenido" => "Ven a conocernos en nuestras instalaciones de Lugo de Llanera, donde elaboramos con mimo cada botella de Sidracola. Te invitamos a descubrir el proceso detrás de nuestra bebida innovadora.",
 ];
 
-$text_quiz_spanish = [];
+$text_quiz_spanish = [
+    "títuloCuestionario" => "¿Cuánto sabes de PHP?",
+    "pregunta" => "Pregunta",
+    "corregir" => "Corregir",
+    "textoNota" => "Has sacado una nota de",
+
+    "pregunta1" => "¿Qué protocolo de PHP envía de manera visible en el navegador?",
+    "pregunta1_resp_a" => "POST",
+    "pregunta1_resp_b" => "GET",
+    "pregunta1_resp_c" => "PANG",
+
+    "pregunta2" => "¿Qué función de PHP se usa para incluir otro fichero PHP?",
+    "pregunta2_resp_a" => "include",
+    "pregunta2_resp_b" => "insert-file",
+    "pregunta2_resp_c" => "import_contents()",
+
+    "pregunta3" => "¿Cómo podemos asignar una variable además de por valor?",
+    "pregunta3_resp_a" => "Por diferencia",
+    "pregunta3_resp_b" => "Por referencia",
+    "pregunta3_resp_c" => "Por puntero cartesiano",
+
+    "pregunta4" => "¿Cómo declaramos una función en PHP?",
+    "pregunta4_resp_a" => "script()",
+    "pregunta4_resp_b" => "declare-function",
+    "pregunta4_resp_c" => "function nombreFuncion()",
+
+    "pregunta5" => "¿Cómo hacemos para devolver un valor en una función?",
+    "pregunta5_resp_a" => "return",
+    "pregunta5_resp_b" => "total_sum",
+    "pregunta5_resp_c" => "give-value-back",
+
+    "pregunta6" => "¿Cómo pasamos un parámetro por referencia?",
+    "pregunta6_resp_a" => '$parametro(ref)',
+    "pregunta6_resp_b" => "start_param->new_param",
+    "pregunta6_resp_c" => '&$parametro',
+
+    "pregunta7" => "¿Cómo se llama la función que permite obtener el valor de una variable global en PHP?",
+    "pregunta7_resp_a" => "global",
+    "pregunta7_resp_b" => '$_GLOBALS',
+    "pregunta7_resp_c" => 'worldVar',
+
+    "pregunta8" => "¿Cuál es la diferencia entre include y require?",
+    "pregunta8_resp_a" => "Con include si el archivo no existe o da error, la ejecución se para",
+    "pregunta8_resp_b" => "Con require si el archivo no existe o da error, la ejecución se para",
+    "pregunta8_resp_c" => "Hacen lo mismo",
+
+    "pregunta9" => "¿Cuántas veces carga un archivo el comando include_once?",
+    "pregunta9_resp_a" => "1",
+    "pregunta9_resp_b" => "Infinitas, cada vez que se usa",
+    "pregunta9_resp_c" => "11",
+
+    "pregunta10" => "¿Qué tipo de llamadas pueden ser agregadas como enlaces a favoritos?",
+    "pregunta10_resp_a" => "POST",
+    "pregunta10_resp_b" => "PUAGH",
+    "pregunta10_resp_c" => "GET",
+];
 
 
 #english
 $text_index_english = [
-    "usuario" => "User", 
-    "contraseña" => "Password",  
+    "usuario" => "User",
+    "contraseña" => "Password",
     "iniciar_sesion" => "Log in",
     "usuario_pass_incorr" => "Incorrect username and/or password",
     "intentos_restantes" => "Remaining attempts:"
 ];
 
 $text_main_english = [
-    "salir" => "Log out",    
+    "salir" => "Log out",
     "inicio" => "Home",
-    "cuestionario" => "Quiz",    
+    "cuestionario" => "Quiz",
     "info" => "Information",
     "nosotros" => "About Us",
     "nosotros_contenido" => "Javi and Marta are two young entrepreneurs with a background in Chemistry and Business. Their passion for innovation and sustainability led them to create Sidracola, a unique and natural beverage.",
@@ -47,12 +102,67 @@ $text_main_english = [
     "ven_a_vernos_contenido" => "Come and meet us at our facilities in Lugo de Llanera, where we carefully craft every bottle of Sidracola. We invite you to discover the process behind our innovative drink.",
 ];
 
-$text_quiz_english = [];
+$text_quiz_english = [
+    "títuloCuestionario" => "How much do you know about PHP?",
+    "pregunta" => "Question",
+    "corregir" => "Correct",
+    "textoNota" => "You scored",
+
+    "pregunta1" => "Which PHP protocol sends data visibly in the browser?",
+    "pregunta1_resp_a" => "POST",
+    "pregunta1_resp_b" => "GET",
+    "pregunta1_resp_c" => "PANG",
+
+    "pregunta2" => "Which PHP function is used to include another PHP file?",
+    "pregunta2_resp_a" => "include",
+    "pregunta2_resp_b" => "insert-file",
+    "pregunta2_resp_c" => "import_contents()",
+
+    "pregunta3" => "How can we assign a variable besides by value?",
+    "pregunta3_resp_a" => "By difference",
+    "pregunta3_resp_b" => "By reference",
+    "pregunta3_resp_c" => "By Cartesian pointer",
+
+    "pregunta4" => "How do we declare a function in PHP?",
+    "pregunta4_resp_a" => "script()",
+    "pregunta4_resp_b" => "declare-function",
+    "pregunta4_resp_c" => "function functionName()",
+
+    "pregunta5" => "How do we return a value in a function?",
+    "pregunta5_resp_a" => "return",
+    "pregunta5_resp_b" => "total_sum",
+    "pregunta5_resp_c" => "give-value-back",
+
+    "pregunta6" => "How do we pass a parameter by reference?",
+    "pregunta6_resp_a" => '$parameter(ref)',
+    "pregunta6_resp_b" => "start_param->new_param",
+    "pregunta6_resp_c" => '&$parameter',
+
+    "pregunta7" => "What is the function to get the value of a global variable in PHP?",
+    "pregunta7_resp_a" => "global",
+    "pregunta7_resp_b" => '$_GLOBALS',
+    "pregunta7_resp_c" => 'worldVar',
+
+    "pregunta8" => "What is the difference between include and require?",
+    "pregunta8_resp_a" => "With include, if the file doesn't exist or gives an error, execution stops",
+    "pregunta8_resp_b" => "With require, if the file doesn't exist or gives an error, execution stops",
+    "pregunta8_resp_c" => "They do the same thing",
+
+    "pregunta9" => "How many times does the include_once command load a file?",
+    "pregunta9_resp_a" => "1",
+    "pregunta9_resp_b" => "Infinite, every time it is used",
+    "pregunta9_resp_c" => "11",
+
+    "pregunta10" => "What type of requests can be added as favorites links?",
+    "pregunta10_resp_a" => "POST",
+    "pregunta10_resp_b" => "PUAGH",
+    "pregunta10_resp_c" => "GET",
+];
 
 
 #french
 $text_index_french = [
-    "usuario" => "Utilisateur", 
+    "usuario" => "Utilisateur",
     "contraseña" => "Mot de passe",
     "iniciar_sesion" => "Se connecter",
     "usuario_pass_incorr" => "Nom d'utilisateur et/ou mot de passe incorrects",
@@ -60,9 +170,9 @@ $text_index_french = [
 ];
 
 $text_main_french = [
-    "salir" => "Se déconnecter",    
+    "salir" => "Se déconnecter",
     "inicio" => "Accueil",
-    "cuestionario" => "Quiz",    
+    "cuestionario" => "Quiz",
     "info" => "Informations",
     "nosotros" => "À propos de nous",
     "nosotros_contenido" => "Javi et Marta sont deux jeunes entrepreneurs diplômés en chimie et en gestion d'entreprise. Leur passion pour l'innovation et la durabilité les a conduits à créer Sidracola, une boisson naturelle et unique.",
@@ -72,6 +182,59 @@ $text_main_french = [
     "ven_a_vernos_contenido" => "Venez nous rencontrer dans nos installations à Lugo de Llanera, où nous fabriquons soigneusement chaque bouteille de Sidracola. Nous vous invitons à découvrir le processus derrière notre boisson innovante.",
 ];
 
-$text_quiz_french = [];
+$text_quiz_french = [
+    "títuloCuestionario" => "Combien savez-vous de PHP?",
+    "pregunta" => "Question",
+    "corregir" => "Corriger",
+    "textoNota" => "Vous avez obtenu un score de",
 
-?>
+    "pregunta1" => "Quel protocole PHP envoie-t-il des données de manière visible dans le navigateur ?",
+    "pregunta1_resp_a" => "POST",
+    "pregunta1_resp_b" => "GET",
+    "pregunta1_resp_c" => "PANG",
+
+    "pregunta2" => "Quelle fonction PHP est utilisée pour inclure un autre fichier PHP ?",
+    "pregunta2_resp_a" => "include",
+    "pregunta2_resp_b" => "insert-file",
+    "pregunta2_resp_c" => "import_contents()",
+
+    "pregunta3" => "Comment pouvons-nous assigner une variable en plus par valeur ?",
+    "pregunta3_resp_a" => "Par différence",
+    "pregunta3_resp_b" => "Par référence",
+    "pregunta3_resp_c" => "Par pointeur cartésien",
+
+    "pregunta4" => "Comment déclarons-nous une fonction en PHP ?",
+    "pregunta4_resp_a" => "script()",
+    "pregunta4_resp_b" => "declare-function",
+    "pregunta4_resp_c" => "function nomFonction()",
+
+    "pregunta5" => "Comment renvoyer une valeur dans une fonction ?",
+    "pregunta5_resp_a" => "return",
+    "pregunta5_resp_b" => "total_sum",
+    "pregunta5_resp_c" => "give-value-back",
+
+    "pregunta6" => "Comment passons-nous un paramètre par référence ?",
+    "pregunta6_resp_a" => '$paramètre(ref)',
+    "pregunta6_resp_b" => "start_param->new_param",
+    "pregunta6_resp_c" => '&$paramètre',
+
+    "pregunta7" => "Comment appelle-t-on la fonction qui permet d'obtenir la valeur d'une variable globale en PHP ?",
+    "pregunta7_resp_a" => "global",
+    "pregunta7_resp_b" => '$_GLOBALS',
+    "pregunta7_resp_c" => 'worldVar',
+
+    "pregunta8" => "Quelle est la différence entre include et require ?",
+    "pregunta8_resp_a" => "Avec include, si le fichier n'existe pas ou donne une erreur, l'exécution s'arrête",
+    "pregunta8_resp_b" => "Avec require, si le fichier n'existe pas ou donne une erreur, l'exécution s'arrête",
+    "pregunta8_resp_c" => "Ils font la même chose",
+
+    "pregunta9" => "Combien de fois la commande include_once charge-t-elle un fichier ?",
+    "pregunta9_resp_a" => "1",
+    "pregunta9_resp_b" => "Infinie, chaque fois qu'elle est utilisée",
+    "pregunta9_resp_c" => "11",
+
+    "pregunta10" => "Quel type d'appels peuvent être ajoutés comme liens favoris ?",
+    "pregunta10_resp_a" => "POST",
+    "pregunta10_resp_b" => "PUAGH",
+    "pregunta10_resp_c" => "GET",
+];
