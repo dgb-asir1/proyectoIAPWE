5e00f0c8289b48b993b27e620eb91de30995730e
dgb-asir1 <damasogb58@educastur.es>
2025-11-15 23:49:55 +0100

    se quita nav de pagina de error

--- loginError.php
+++ loginError.php
@@ -21,53 +21,7 @@ require_once('scripts/logout.php');
     <link rel="stylesheet" href="css/lightTheme.css">
 </head>
 
-<body>
-    <nav id="nav1">
-        <div id="languageBtns">
-            <form action="" method="get">
-                <input type="hidden" name="lang" value="ESP">
-                <button id="langBtnEs" type="submit"><img src="./img/flag_Spain.png"></button>
-            </form>
-
-            <form action="" method="get">
-                <input type="hidden" name="lang" value="ENG">
-                <button id="langBtnEng" type="submit"><img src="./img/flag_USA.png"></button>
-            </form>
-
-            <form action="" method="get">
-                <input type="hidden" name="lang" value="FRA">
-                <button id="langBtnFr" type="submit"><img src="./img/flag_France.png"></button>
-            </form>
-        </div>
-
-        </div>
-    </nav>
-    <nav id="nav2">
-        <div id="logo"><button><img src="./img/logo.png"></img></button></div>
-        <div id="mainBtns">
-            <button><a href="mainContent.php">
-                    <?php echo $text_main['inicio'] ?>
-                </a></button>
-            <button><a href="cuestionario.php">
-                    <?php echo $text_main['cuestionario'] ?>
-                </a></button>
-        </div>
-        <div id="themeBtn">
-            <form action="" method="get">
-                <input type="hidden" name="theme" value=
-                    <?php 
-                    if(isset($_GET['theme'])){
-                        $_GET['theme'] == 'LIGHT' ? $themeBtnValue = "DARK" : $themeBtnValue = "LIGHT";   
-                    }
-                    else {
-                        $themeBtnValue = "DARK";
-                    }
-                    echo $themeBtnValue;
-                    ?>>
-                <button type="submit"><img src="./img/themeSwitch.png"></img></button>
-            </form>
-        </div>
-    </nav>    
+<body> 
     <section>
         <h3>Error</h3>
         <p>Ha excedido el número máximo permitido de intentos de sesión</p>
